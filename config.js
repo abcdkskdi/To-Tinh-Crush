73ccf351d2843b62efdb6f3eb88e7fe3ba6948c5
abcdkskdi <88835604+abcdkskdi@users.noreply.github.com>
2021-08-12 20:46:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Này Huyyyyyyyy 😗😗😗',
-    introDesc: 'Thấy Huy lạc trong khu rừng nhỏ
-    baby Huy đừng lo  🙄🙄🙄
+    introDesc: `Thấy Huy lạc trong khu rừng nhỏ
+    Baby Huy đừng lo  🙄🙄🙄
     Huy nói Huy thích con thỏ ok để đó t bắt dùm cho 
     Không cần hỏi không cần tò mò
     Chỉ cần biết t thích Huy từ nhỏ 👉👈
     Không biết diễn tả bằng từ gì 
-    Nhưng trước giờ t chưa nói từ bỏ', 
+    Nhưng trước giờ t chưa nói từ bỏ `, 
     btnIntro: 'Hic hic 😭😭',
     title: 'Thoát ra làm chóoo 😠😠😠',
     desc: ' Huy thấy t có đáng iu khưm ?',
     btnYes: 'Đáng iu lắm lun 🤗🤗',
     btnNo: 'Hong, nhìn thiểu năng ghê',
     question:'Vì sao Huy không rep tin nhắn của t z 😢😢',
-    btnReply: 'trả lời thiệt lòng nhaaa 😳😳😳',
+    btnReply: 'Trả lời thiệt lòng nhaaa 😳😳😳',
     reply: 'Vì Huy quên khưm rep hứ k phải do ghét m đâu 🙁🙁',
     mess: 'T biết mà, hihi 😚😚',
     messDesc: 'Vậy thì đừng seen tin nhắn nữa đồ lạnh lùng :<',
